b7674e9820477269cf7b9133ffc800c68f8f814b
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-05-14 15:15:30 +1200

    Added Week 1 Assignments

---
+++ python-basics/main.py
@@ -0,0 +1,81 @@
+### Values and Data Types
+#a = 100
+#b = 3.14
+#c = 'Hello World'
+#print(type(a))
+#print(type(b))
+#print(type(c))
+
+### Operators and Operands
+#print(100+200) #Addition
+#print(100-200) #Subtraction
+#print(2*2) #Multiplication
+#print(10/3) #Division
+#print(10//3) #Remeinder
+#print(10%3) #Modular
+#print(4**2) #Exponentiation
+
+## Precedence
+# 1. Parentheses
+# 2. Multiplication & Division
+# 3. Addition and Subtraction
+
+### Functions Calls
+#input (arguments)
+#output (return value)
+
+### Type Conversion Functions
+#print(3.14, int(3.14))
+#print(3.9999, int(3.9999))
+#print("2345", int("2345"))
+#print(17, int(17))
+#print(int("23"))
+#rint(float("123.45"))
+#print(type(float("123.45")))
+#print(str(123.45))
+#print(type(str(123.45)))
+
+### Variables - Useful for storing values of expressions and use later on
+#message = "What's up, Doc?"
+#n = 17 
+#pi = 3.141559
+#print(message)
+#print(n)
+#print(pi)
+ 
+### Python Keywords
+# and, as, assert, break, class, continue, def, del, elif, else, except, exec, finally, for, from, global
+# if, import, in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+# True, False, None 
+
+### Statements and Expressions
+# Assignment Statement (pi=3.14)
+# Assignment Statement of an expression ( value = 3.14 + 1)
+# Python sequencially evaluate expressions, creating sub expressions
+
+### Updating Variables
+#x +=3 same as x = x + 3 
+#y -=1 same as y = y - 1
+
+
+### Turtle Graphics
+#import turtle
+#wn = turtle.Screen()
+#alex = turtle.Turtle()
+#alex.forward(150)
+#alex.left(90)
+#alex.forward(75)
+
+### Repetition with a For Loop
+#for _ in range(3):
+#    print(_)
+
+
+### Importing Modules
+#import random
+from random import randrange, random
+prob = random()
+print(prob)
+
+diceThrow = randrange(1,7)
+print(diceThrow)
