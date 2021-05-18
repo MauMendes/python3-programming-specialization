542c90b24c40b14ac92073815638f3d1581b55ee
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-05-19 11:49:17 +1200

    Added Week 4 Assignment 2

--- python-basics/week4-assignment2.py
+++ python-basics/week4-assignment2.py
@@ -1,9 +1,31 @@
-#1) 
+#1) seqmut-1-2: Which method would you use to figure out the position of an item in a list?
+#D. .index()
 
-#2) 
+#2) seqmut-1-3: Which method is best to use when adding an item to the end of a list?
+# C. .append()
 
-#3) 
+#3) Write code to add ‘horseback riding’ to the third position (i.e., right before volleyball) in the list sports.
+#sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+#sports.insert(2, 'horseback riding')
+#print(sports)
 
-#4) 
+#4) Write code to take ‘London’ out of the list trav_dest.
+#rav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+#trav_dest.remove('London')
+#print(trav_dest)
 
-#5) 
+#5) Write code to add ‘Guadalajara’ to the end of the list trav_dest using a list method.
+#trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+#trav_dest.append('Guadalajara')
+#print(trav_dest)
+
+#6) Write code to rearrange the strings in the list winners so that they are in alphabetical order from A to Z.
+#winners = ['Kazuo Ishiguro', 'Rainer Weiss', 'Youyou Tu', 'Malala Yousafzai', 'Alice Munro', 'Alvin E. Roth']
+#winners.sort()
+#print(winners)
+
+#7) Write code to switch the order of the winners list so that it is now Z to A. Assign this list to the variable z_winners.
+#winners = ['Alice Munro', 'Alvin E. Roth', 'Kazuo Ishiguro', 'Malala Yousafzai', 'Rainer Weiss', 'Youyou Tu']
+#z_winners = winners[:]
+#z_winners.reverse()
+#print(z_winners)
