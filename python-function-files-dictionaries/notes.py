219549768c5dec5bb37f72312b64cbd1b6a5b283
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-05-24 16:13:58 +1200

    Added week 5 Assignment 1

--- python-function-files-dictionaries/notes.py
+++ python-function-files-dictionaries/notes.py
@@ -339,3 +339,63 @@
 #separate function definitions with two blank lines
 #keep function definitions together
 #keep top level statements, including function calls, together at the bottom of the program
+
+#### WEEK 5 - SORTING BASICS
+
+## method sorted
+## or list.sort()
+## optional parameters
+#L2 = ["Cherry", "Apple", "Blueberry"]
+#print(sorted(L2, reverse=True))
+#lst = [3, 5, 1, 6, 7, 2, 9, -2, 5]
+#lst_sorted = sorted(lst, reverse=True)
+
+## optional key parameters
+#L1 = [1, 7, 4, -2, 3]
+#def absolute(x):
+#    if x >= 0:
+#        return x
+#    else:
+#        return -x
+#
+#L2 = sorted(L1, key=absolute)
+#print(L2)
+##or in reverse order
+#print(sorted(L1, reverse=True, key=absolute))
+
+# You will be sorting the following list by each element’s second letter, a to z. Create a function to use when sorting, 
+# called second_let. It will take a string as input and return the second letter of that string. Then sort the list, create a 
+# variable called sorted_by_second_let and assign the sorted list to it. Do not use lambda.
+#ex_lst = ['hi', 'how are you', 'bye', 'apple', 'zebra', 'dance']
+#def second_let(str):
+#    return str[1]
+#sorted_by_second_let  = sorted(ex_lst, key=second_let)
+#print(sorted_by_second_let)
+
+# Below, we have provided a list of strings called nums. Write a function called last_char that takes a string as input, and returns 
+# only its last character. Use this function to sort the list nums by the last digit of each number, from highest to lowest, and save 
+# this as a new list called nums_sorted.
+#nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+#def last_char(str):
+#    return str[-1]
+#nums_sorted = sorted(nums, reverse=True, key=last_char)
+#print(nums_sorted)
+
+#Once again, sort the list nums based on the last digit of each number from highest to lowest. However, now you should do so by 
+# writing a lambda function. Save the new list as nums_sorted_lambda.
+#nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+#nums_sorted_lambda = sorted(nums, reverse=True, key=lambda ch:ch[-1])
+
+#### ADVANCED SORTED W/ dictionaries
+#Sort the following dictionary based on the keys so that they are sorted a to z. Assign the resulting value to the variable sorted_keys
+#dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+#sorted_keys = sorted(dictionary.keys())
+
+#Sort the following dictionary’s keys based on the value from highest to lowest. Assign the resulting value to the variable sorted_values.
+#dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+#sorted_values = list()
+#
+# now loop through the sorted keys
+#for k in sorted(dictionary, key=lambda k: dictionary[k], reverse=True):
+#    print("{} Have {} Items".format(k, dictionary[k]))
+#    sorted_values.append(k)
