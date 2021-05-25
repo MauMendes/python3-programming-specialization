ec3b34f0289f7e30a6a6826b1d58bcc4e589cdd9
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-05-25 15:56:59 +1200

    Starting Python Data Collection

--- python-data-collection-and-processing/notes.py
+++ python-data-collection-and-processing/notes.py
@@ -1,1 +1,114 @@
-#### WEEK 1
+#### WEEK 1 - NESTED DATA
+animals = [['cat', 'dog', 'mouse'], ['horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+idx1 = animals[1][0] #get only horse
+
+data = ['bagel', 'cream cheese', 'breakfast', 'grits', 'eggs', 'bacon', [34, 9, 73, []], [['willow', 'birch', 'elm'], 'apple', 'peach', 'cherry']]
+plant = data[7][0][0] # get 'willow' only
+
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+      }
+color = info['personal_data']['physical_features']['color']
+
+### JSON Format and JSON Module
+json.loads(string) #turns a json-formatted string into a list or dictionary
+import json
+a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
+print(a_string)
+d = json.loads(a_string)
+
+json.dumps(dictionary) #dump a object to a string
+import json
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
+d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2':{'b': 3, 'c': "yes"}}
+print(pretty(d))
+
+### Nested Iteration
+nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    for y in x:
+        print("     level2: " + y)
+
+info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig', 1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
+last_names = list()
+for people in info:
+    last_names.append(people[1])
+
+L = [['apples', 'bananas', 'oranges', 'blueberries', 'lemons'], ['carrots', 'peas', 'cucumbers', 'green beans'], ['root beer', 'smoothies', 'cranberry juice']]
+b_strings = list()
+for item in L:
+    for item2 in item:
+        if 'b' in item2:
+            b_strings.append(item2)
+
+# A MISC OF LISTS and DATA in a LIST
+nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+for x in nested1:
+    print("level1: ")
+    if type(x) is list:
+        for y in x:
+            print("     level2: {}".format(y))
+    else:
+        print(x)
+
+#SHALOW & DEEP COPIES
+
+# copied version still have a connection with the original
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_version = original[:]
+print(copied_version)
+print(copied_version is original)
+print(copied_version == original)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_version)
+
+# Copie the list 
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = []
+    for item in inner_list:
+        copied_inner_list.append(item)
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
+
+# Be really careful with this THING!
+import copy
+original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+shallow_copy_version = original[:]
+deeply_copied_version = copy.deepcopy(original)
+original.append("Hi there")
+original[0].append(["marsupials"])
+print("-------- Original -----------")
+print(original)
+print("-------- deep copy -----------")
+print(deeply_copied_version)
+print("-------- shallow copy -----------")
+print(shallow_copy_version)
+
+#output
+#-------- Original -----------
+#[['canines', ['dogs', 'puppies'], ['marsupials']], ['felines', ['cats', 'kittens']], 'Hi there']
+#-------- deep copy -----------
+#[['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+#-------- shallow copy -----------
+#[['canines', ['dogs', 'puppies'], ['marsupials']], ['felines', ['cats', 'kittens']]]
