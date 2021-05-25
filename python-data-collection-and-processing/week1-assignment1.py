8996ffe1a64de493fc90dc0e4882e3f982e78078
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-05-25 16:02:28 +1200

    updated assignment

--- python-data-collection-and-processing/week1-assignment1.py
+++ python-data-collection-and-processing/week1-assignment1.py
@@ -65,15 +65,18 @@ for key in nested_d.keys():
 #6) Below, we have provided a nested dictionary. Index into the dictionary to create variables that we have listed in the 
 # ActiveCode window. ???????????????????????????????????
 sports = {'swimming': ['butterfly', 'breaststroke', 'backstroke', 'freestyle'], 'diving': ['springboard', 'platform', 'synchronized'], 'track': ['sprint', 'distance', 'jumps', 'throws'], 'gymnastics': {'women':['vault', 'floor', 'uneven bars', 'balance beam'], 'men': ['vault', 'parallel bars', 'floor', 'rings']}}
-
 # Assign the string 'backstroke' to the name v1
-v1 = 'backstroke' #?????????????????????
+v1= sports['swimming'][1]
+v1
+
 # Assign the string 'platform' to the name v2
-v2 = 'platform'#?????????????????????
+v2= sports['diving'][1]
+v2
 # Assign the list ['vault', 'floor', 'uneven bars', 'balance beam'] to the name v3
-v3 = ['vault', 'floor', 'uneven bars', 'balance beam'] #?????????????????????
+v3=sports['gymnastics']['women']
 # Assign the string 'rings' to the name v4
-v4 = 'rings'#?????????????????????
+v4=sports['gymnastics']['men'][3]
+v4
 
 #7) Given the dictionary, nested_d, save the medal count for the USA from all three Olympics in the dictionary to the list US_count.
 nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
