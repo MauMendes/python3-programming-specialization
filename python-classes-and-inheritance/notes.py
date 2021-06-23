1801c6d19caf72070f6d43e68000759bf3321984
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-06-24 11:43:10 +1200

    added example city

--- python-classes-and-inheritance/notes.py
+++ python-classes-and-inheritance/notes.py
@@ -66,3 +66,30 @@
 #
 #spider = Animal(4,4)
 #pidlimbs = spider.limbs()
+
+#cityNames =['Detroit', 'Ann Arbour', 'Pittsburgh', 'Mars', 'New York']
+#populations = [680250,117070,304391,1683,8406000]
+#states = ['MI','MI','PA','PA','NY']
+#city_tuples = zip(cityNames, populations, states)
+#print(str(city_tuples))
+
+#class City:
+#    def __init__(self, n, p, s):
+#        self.name = n
+#        self.population = p
+#        self.state = s
+#    def __str__(self):
+#        return '{},{},(pop:{})'.format(self.name, self.state, self.population)
+
+#cities =[]
+#for city_tup in city_tuples:
+#    n,p,s = city_tup;
+#    city = City(n,p,s)
+#    cities.append(city)
+#    print(city)
+#print(cities)
+
+#all of that using list comprehesions
+#cities = [City(n,p,s) for (n,p,s) in city_tuples]
+#cities = [City(*t) for t in city_tuples]
+#print(cities)
