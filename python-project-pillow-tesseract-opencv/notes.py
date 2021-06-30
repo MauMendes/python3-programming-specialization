b6d18a8c2baa2c1e8ab5e9d8d720911bd8babf4d
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-06-30 14:53:20 +1200

    adding updated notes and jupyter notebook

--- python-project-pillow-tesseract-opencv/notes.py
+++ python-project-pillow-tesseract-opencv/notes.py
@@ -1,1 +1,7 @@
-#### WEEK 1
+#### WEEK 1 - 
+# Pillow - Image Manipulation
+# Tesseract -
+# Kraken - Layout images to caracters
+# OpenCV - detect faces
+# Jupyter Notebooks - C:\Python38\python\python.exe
+
