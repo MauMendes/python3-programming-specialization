9c5bd3f20aeb18ff108b0b59a4b5de7bedeb6354
Mauricio Mendes <mauricio@hardcode.com.br>
2021-07-02 18:43:54 +1200

    updated week 1 notes

--- python-project-pillow-tesseract-opencv/notes.py
+++ python-project-pillow-tesseract-opencv/notes.py
@@ -1,1 +1,8 @@
+#### How to Install Jupyter on Windows
+# Install Python - wwww.python.org/downloads
+# package mananger - https://www.anaconda.com/distribution
+# create virtual environment - conda create -n py3env python=3.8.5 anaconda
+# activate virtual environment - source activate py3env, conda deactivate
+
 #### WEEK 1
+#Pillow
